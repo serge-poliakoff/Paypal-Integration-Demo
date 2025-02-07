5dc046b7bf07e3516d66f3330a2d2d0b5dab572c
Serge Poliakov <poliakov.serega@gmail.com>
2025-02-07 20:01:49 +0300

    Changing some shit that I've done

--- EF_Example2/EF_Example2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EF_Example2.Data;
-using EF_Example2.Models;
-using System.Net.Http.Headers;
-using System.Text;
-
-ZernoContext context = new ZernoContext();
-
-Random rnd = new Random();
